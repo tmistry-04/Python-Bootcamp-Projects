70865f40ac3089bc2f0ae702862192399d553d84
tanisha <t3mistry@uwaterloo.ca>
2026-04-08 13:22:44 -0400

    Spotify Monthly Listeners Game

---
+++ Spotify Monthly Listeners/game_data.py
@@ -0,0 +1,173 @@
+# Dataset created for educational purposes.
+# Spotify monthly listener counts are approximate
+
+data = [
+    {
+        "name": "Taylor Swift",
+        "monthly_listeners": 87,
+        "description": "pop/country singer-songwriter",
+        "country": "USA"
+    },
+    {
+        "name": "Drake",
+        "monthly_listeners": 80,
+        "description": "rap and hip-hop artist",
+        "country": "Canada"
+    },
+    {
+        "name": "Bad Bunny",
+        "monthly_listeners": 92,
+        "description": "reggaeton and Latin trap artist",
+        "country": "Puerto Rico"
+    },
+    {
+        "name": "Billie Eilish",
+        "monthly_listeners": 108,
+        "description": "alternative pop singer-songwriter",
+        "country": "USA"
+    },
+    {
+        "name": "The Weeknd",
+        "monthly_listeners": 126,
+        "description": "R&B and pop artist",
+        "country": "Canada"
+    },
+    {
+        "name": "Dua Lipa",
+        "monthly_listeners": 64,
+        "description": "pop and dance-pop singer",
+        "country": "UK"
+    },
+    {
+        "name": "Harry Styles",
+        "monthly_listeners": 48,
+        "description": "pop and rock singer-songwriter",
+        "country": "UK"
+    },
+    {
+        "name": "Olivia Rodrigo",
+        "monthly_listeners": 44,
+        "description": "pop and alternative singer-songwriter",
+        "country": "USA"
+    },
+    {
+        "name": "Kendrick Lamar",
+        "monthly_listeners": 105,
+        "description": "rapper and record producer",
+        "country": "USA"
+    },
+    {
+        "name": "Sabrina Carpenter",
+        "monthly_listeners": 72,
+        "description": "pop singer-songwriter",
+        "country": "USA"
+    },
+    {
+        "name": "SZA",
+        "monthly_listeners": 96,
+        "description": "R&B and soul singer-songwriter",
+        "country": "USA"
+    },
+    {
+        "name": "Post Malone",
+        "monthly_listeners": 70,
+        "description": "rap and pop artist",
+        "country": "USA"
+    },
+    {
+        "name": "Coldplay",
+        "monthly_listeners": 93,
+        "description": "alternative rock band",
+        "country": "UK"
+    },
+    {
+        "name": "Ariana Grande",
+        "monthly_listeners": 83,
+        "description": "pop and R&B singer",
+        "country": "USA"
+    },
+    {
+        "name": "Ed Sheeran",
+        "monthly_listeners": 74,
+        "description": "pop and acoustic singer-songwriter",
+        "country": "UK"
+    },
+    {
+        "name": "Rosalia",
+        "monthly_listeners": 18,
+        "description": "flamenco and urban pop artist",
+        "country": "Spain"
+    },
+    {
+        "name": "Burna Boy",
+        "monthly_listeners": 22,
+        "description": "Afrobeats and reggae artist",
+        "country": "Nigeria"
+    },
+    {
+        "name": "Tyla",
+        "monthly_listeners": 14,
+        "description": "Afropop and R&B singer",
+        "country": "South Africa"
+    },
+    {
+        "name": "Peso Pluma",
+        "monthly_listeners": 35,
+        "description": "regional Mexican and corridos tumbados artist",
+        "country": "Mexico"
+    },
+    {
+        "name": "Gracie Abrams",
+        "monthly_listeners": 12,
+        "description": "indie pop singer-songwriter",
+        "country": "USA"
+    },
+    {
+        "name": "Queen",
+        "monthly_listeners": 53,
+        "description": "legendary rock band",
+        "country": "UK"
+    },
+    {
+        "name": "Elliott Smith",
+        "monthly_listeners": 3,
+        "description": "indie folk and alternative singer-songwriter",
+        "country": "USA"
+    },
+    {
+        "name": "Radiohead",
+        "monthly_listeners": 45,
+        "description": "alternative and art rock band",
+        "country": "UK"
+    },
+    {
+        "name": "Muse",
+        "monthly_listeners": 16,
+        "description": "alternative rock and progressive band",
+        "country": "UK"
+    },
+    {
+        "name": "Green Day",
+        "monthly_listeners": 36,
+        "description": "punk rock band",
+        "country": "USA"
+    },
+    {
+        "name": "The Smashing Pumpkins",
+        "monthly_listeners": 11,
+        "description": "alternative rock band",
+        "country": "USA"
+    },
+    {
+        "name": "Geese",
+        "monthly_listeners": 2,
+        "description": "indie rock band",
+        "country": "USA"
+    },
+    {
+        "name": "My Chemical Romance",
+        "monthly_listeners": 19,
+        "description": "alternative and emo rock band",
+        "country": "USA"
+    },
+]
